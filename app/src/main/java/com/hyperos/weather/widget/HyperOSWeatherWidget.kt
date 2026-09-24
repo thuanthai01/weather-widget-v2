6db90b49563c9ae1b6180a172f40d06231795d1f
thuanthai01 <thuanseo141@gmail.com>
2026-09-24 08:38:43 +0700

    Update HyperOSWeatherWidget.kt

--- app/src/main/java/com/hyperos/weather/widget/HyperOSWeatherWidget.kt
+++ app/src/main/java/com/hyperos/weather/widget/HyperOSWeatherWidget.kt
@@ -15,7 +15,6 @@ import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
-import androidx.glance.layout.defaultWeight
 import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
